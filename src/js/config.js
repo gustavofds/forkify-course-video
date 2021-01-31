11b3bc2b572117ee6474034f86a5d59e08703dca
gustavofds <gustavohl98@gmail.com>
2021-01-31 14:59:13 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '79c4a793-3c3f-4a98-a539-7d17a3c6ae64';
+export const MODAL_CLOSE_SEC = 2.5;
